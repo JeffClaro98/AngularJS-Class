06a877ab8a2f0577ab1b6197f754bc07299f7478
Jeff Claro <Jeffclaro2@gmail.com>
2019-03-31 21:17:02 -0400

    Assignment8
    Having an error where content Id cant be found

---
+++ learning-angular/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {Content} from "./content-card/content-list-helper";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title = 'Hell (AKA Learning-Angular)';
+  constructor() {
+  }
+}
+
